1aa24bcda1a0ce58cc54f572a435fe8aa4e23582
Shawn Lin <xiaoliang.lin@columbia.edu>
2020-06-28 18:27:56 +0000

    improve test run speed by using global vars

--- tests/unit/test_returnseries.py
+++ tests/unit/test_returnseries.py
@@ -3,60 +3,50 @@ import pytest
 import pandas as pd
 from pyform.returnseries import ReturnSeries
 
-
-def test_init():
-
-    ReturnSeries.read_csv("tests/unit/data/twitter_returns.csv")
+returns = ReturnSeries.read_csv("tests/unit/data/twitter_returns.csv")
+spy = ReturnSeries.read_csv("tests/unit/data/spy_returns.csv")
+qqq = ReturnSeries.read_csv("tests/unit/data/qqq_returns.csv")
 
 
 def test_to_week():
 
-    returns = ReturnSeries.read_csv("tests/unit/data/twitter_returns.csv")
     assert returns.to_week().iloc[1, 0] == 0.055942142480424284
 
 
 def test_to_month():
 
-    returns = ReturnSeries.read_csv("tests/unit/data/twitter_returns.csv")
     assert returns.to_month().iloc[1, 0] == 0.5311520760874386
 
 
 def test_to_quarter():
 
-    returns = ReturnSeries.read_csv("tests/unit/data/twitter_returns.csv")
     assert returns.to_quarter().iloc[1, 0] == -0.2667730077753935
 
 
 def test_to_year():
 
-    returns = ReturnSeries.read_csv("tests/unit/data/twitter_returns.csv")
     assert returns.to_year().iloc[1, 0] == -0.4364528678695403
 
 
 def test_to_week_arithmetic():
 
-    returns = ReturnSeries.read_csv("tests/unit/data/twitter_returns.csv")
     assert returns.to_week("arithmetic").iloc[1, 0] == 0.05658200000000001
 
 
 def test_to_week_continuous():
 
-    returns = ReturnSeries.read_csv("tests/unit/data/twitter_returns.csv")
     assert returns.to_week("continuous").iloc[1, 0] == 0.05821338474015869
 
 
 def test_to_period_wrong_method():
 
     with pytest.raises(ValueError):
-        returns = ReturnSeries.read_csv("tests/unit/data/twitter_returns.csv")
         returns.to_freq("W", "contnuuous")  # typo in continuous should cause failure
 
 
 def test_add_benchmark():
 
-    returns = ReturnSeries.read_csv("tests/unit/data/twitter_returns.csv")
-    benchmark = ReturnSeries.read_csv("tests/unit/data/spy_returns.csv")
-    returns.add_benchmark(benchmark, "SPY")
+    returns.add_benchmark(spy, "SPY")
 
     assert "SPY" in returns.benchmark
 
@@ -64,9 +54,6 @@ def test_add_benchmark():
 def test_add_benchmark_no_name():
 
     returns = ReturnSeries.read_csv("tests/unit/data/twitter_returns.csv")
-    spy = ReturnSeries.read_csv("tests/unit/data/spy_returns.csv")
-    qqq = ReturnSeries.read_csv("tests/unit/data/qqq_returns.csv")
-
     returns.add_benchmark(spy)
 
     corr = returns.get_corr()
