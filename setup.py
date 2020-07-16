880473cbdd37cf2f06ddb50054adabc8e3d1e3ad
Shawn Lin <xiaoliang.lin@columbia.edu>
2020-07-16 21:49:06 +0000

    Update dependency

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import versioneer
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+install_reqs = ["pandas>=1.0.0"]
+
 setuptools.setup(
     name="pyform",
     version=versioneer.get_version(),
@@ -15,10 +17,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/shawnlinxl/pyform",
     packages=setuptools.find_packages(),
+    install_requires=install_reqs,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
